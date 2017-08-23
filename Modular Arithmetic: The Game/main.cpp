cd1388cf0970ef0d52da695db0abb1a2d07fe00e
Ross Spencer <rossspencer@rosss-macbook-pro-2.local>
2017-08-23 00:15:38 -0400

    Addition works so far!!

--- Modular Arithmetic: The Game/main.cpp
+++ Modular Arithmetic: The Game/main.cpp
@@ -7,9 +7,43 @@
 //
 
 #include <iostream>
+int addition(int num1, int num2, int modulus);
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World! Ready for some modular arithmetic?\n";
+    std::cout << "Hello, World! Ready for some modular arithmetic?\n" << std::endl;
+    
+    int modulus, userNum, answer;
+    std::cout << "Choose a modulus: ";
+    std::cin >> modulus;
+    std::cout << "Congrats! So far you've chosen " << modulus << " as a modulus: " << std::endl;
+    srand(time(0));
+    int num1 = rand() % 100;
+    int num2 = rand() % 100;
+    int option = 1;
+    switch(option){
+    case 1:
+            std::cout << "What's " << num1 << " + " << num2 << "?" <<std::endl;
+            answer = addition(num1, num2, modulus);
+            std::cin >> userNum;
+            std::cout << "The correct answer is " << answer << " !" << std::endl;
+        
+
+                break;
+        case 2: break;
+        case 3: break;
+        case 4: break;
+    }
+    
     return 0;
+}
+
+int addition(int num1, int num2, int modulus){
+    int answer;
+    answer = num1 + num2;
+    answer %= modulus;
+    
+    
+    return answer;
+    
 }
